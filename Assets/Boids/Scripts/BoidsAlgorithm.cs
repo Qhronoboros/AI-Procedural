74f07615195dfcc2fd561470e83f8cd127d7d1e2
Qhronoboros <47349395+Qhronoboros@users.noreply.github.com>
2026-02-20 02:51:50 +0100

    Added Basic Implementation of Boids

---
+++ Assets/Boids/Scripts/BoidsAlgorithm.cs
@@ -0,0 +1,174 @@
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class Boids : MonoBehaviour
+{
+    [SerializeField] private GameObject boidPrefab;
+
+    [Header("Start Values")]
+    [SerializeField] private int boidStartAmount = 20;
+
+    [SerializeField] private float _startDistanceMin = 0.0f;
+    [SerializeField] private float _startDistanceMax = 100.0f;
+
+    [SerializeField] private float _startVelocityMin = 5.0f;
+    [SerializeField] private float _startVelocityMax = 10.0f;
+
+    private List<Boid> _boids = new List<Boid>();
+
+    class Boid
+    {
+        public Boid(GameObject gameObject, Vector3 position, Vector3 velocity)
+        {
+            this.gameObject = gameObject;
+            this.position = position;
+            this.velocity = velocity;
+        }
+
+        public GameObject gameObject;
+        public Vector3 position;
+        public Vector3 velocity;
+    }
+
+    static Vector3 GetRandomPointInsideSphere(float minimum, float maximum)
+    {
+        Vector3 randomUnitPoint = Random.insideUnitSphere;
+        Vector3 normalizedVector = Vector3.Normalize(randomUnitPoint);
+
+        return randomUnitPoint * (maximum - minimum) + normalizedVector * minimum;
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < boidStartAmount; i++)
+        {
+            // Set positions and velocity
+            Vector3 randomPosition = GetRandomPointInsideSphere(_startDistanceMin, _startDistanceMax);
+            Vector3 randomVelocity = GetRandomPointInsideSphere(_startVelocityMin, _startVelocityMax);
+
+            _boids.Add(new Boid(Instantiate(boidPrefab, randomPosition, Quaternion.identity), randomPosition, randomVelocity));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 v1, v2, v3, v4;
+        int boidsCount = _boids.Count;
+
+        // for (int i = 0; i < boidsCount; i++)
+        // {
+        //     _boids[i].gameObject.transform.position = _boids[i].position;
+        //     Debug.Log(_boids[i].position);
+        // }
+
+        for (int i = 0; i < boidsCount; i++)
+        {
+            Boid boid = _boids[i];
+
+            print(boid.velocity);
+
+            // Cohesion
+            v1 = Cohesion(boid);
+            // Separation
+            v2 = Separation(boid);
+            // Alignment
+            v3 = Alignment(boid);
+
+            // Bound Position
+            v4 = BoundPosition(boid);
+
+            // Add velocities together
+            boid.velocity += v1 + v2 + v3 + v4;
+            // Change position using new velocity
+            boid.position += boid.velocity * Time.deltaTime;
+            boid.gameObject.transform.position = boid.position;
+        }
+    }
+
+    private Vector3 Cohesion(Boid currentBoid)
+    {
+        Vector3 positionSum = Vector3.zero;
+
+        foreach(Boid boid in _boids)
+        {
+            if (boid.gameObject != currentBoid.gameObject)
+            {
+                positionSum += boid.position;
+            }
+        }
+
+        Vector3 positionMean = positionSum / Mathf.Max(_boids.Count - 1, 1);
+
+        return (positionMean - currentBoid.position) / 100.0f;
+    }
+
+    private Vector3 Separation(Boid currentBoid)
+    {
+        Vector3 velocity = Vector3.zero;
+
+        foreach(Boid boid in _boids)
+        {
+            if (boid.gameObject != currentBoid.gameObject)
+            {
+                if (Vector3.Distance(boid.position, currentBoid.position) < 10.0f)
+                {
+                    velocity -= boid.position - currentBoid.position;
+                }
+            }
+        }
+
+        return velocity / 100.0f;
+    }
+
+    private Vector3 Alignment(Boid currentBoid)
+    {
+        Vector3 velocity = Vector3.zero;
+
+        foreach(Boid boid in _boids)
+        {
+            if (boid.gameObject != currentBoid.gameObject)
+            {
+                velocity += boid.velocity;
+            }
+        }
+
+        Vector3 velocityMean = velocity / Mathf.Max(_boids.Count - 1, 1);
+
+        return (velocityMean - currentBoid.velocity) / 8.0f;
+    }
+
+    private Vector3 BoundPosition(Boid currentBoid)
+    {
+        Vector3 velocity = Vector3.zero;
+
+        if (currentBoid.position.x < -100.0f)
+        {
+            velocity.x = 5;
+        }
+        else if (currentBoid.position.x > 100.0f)
+        {
+            velocity.x = -5;
+        }
+
+        if (currentBoid.position.y < -100.0f)
+        {
+            velocity.y = 5;
+        }
+        else if (currentBoid.position.y > 100.0f)
+        {
+            velocity.y = -5;
+        }
+
+        if (currentBoid.position.z < -100.0f)
+        {
+            velocity.z = 5;
+        }
+        else if (currentBoid.position.z > 100.0f)
+        {
+            velocity.z = -5;
+        }
+
+        return velocity;
+    }
+}
